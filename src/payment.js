bc11550b0aeb244074a7036b1e377081d2070511
Maxim <mamaama@mail.ru>
2026-01-31 19:49:30 +1000

    Use currency-specific commission rates

--- src/payment.js
+++ src/payment.js
@@ -10,4 +10,17 @@ function processPayment(amount, currency) {
     };
 }
 
+const rates = require('../config/rates.json');
+
+function processPayment(amount, currency) {
+    const commission = rates[currency] || 0.02;
+    const total = amount * (1 + commission);
+    return {
+        amount: amount,
+        commission: amount * commission,
+        total: total,
+        currency: currency
+    };
+}
+
 module.exports = { processPayment };
